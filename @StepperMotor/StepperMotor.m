feb9d195b66f45a69bf0ecb40ec24edc3db4daae
akean <akean@C18HPTG.client.chalmers.se>
2022-03-31 11:33:33 +0200

    StepperMotor can now have multiple instances from the same controller.

--- @StepperMotor/StepperMotor.m
+++ @StepperMotor/StepperMotor.m
@@ -1,7 +1,7 @@
 classdef StepperMotor
-    properties (SetAccess = private, Hidden)
-        sp internal.Serialport
-        ax uint8
+    properties (SetAccess = private)
+        port string
+        axis uint8
     end
     methods
         %% CONSTRUCTOR
@@ -9,8 +9,11 @@ classdef StepperMotor
             assert(isscalar(port) && isstring(port))
             assert(isscalar(axis) && any(1:6 == axis))
 
-            obj.sp = serialport(port,9600,"Timeout",1);
-            obj.ax = axis;
+            global StepperMotorPortMap %#ok<GVMIS> 
+            if not( StepperMotorPortMap.isKey(port) ) 
+                StepperMotorPortMap(port) = serialport(port,9600,"Timeout",1);
+            end
+            obj.axis = axis;
 
             % 3.7.37 Request Target Position reached Event
             % byteString = encodeCommand(1,138,1,obj.ax,int32(2^obj.ax));
@@ -29,12 +32,12 @@ classdef StepperMotor
             if nargin<3, timeout = 100; end
             obj.Set("ReferenceSearchMode",int32(1))
             % 3.7.13 Reference Search START
-            byteString = encodeCommand(1,13,0,obj.ax,int32(0));
+            byteString = encodeCommand(1,13,0,obj.axis,int32(0));
             obj.writeread(byteString);
             tic
             while toc<timeout
                 % 3.7.13 Reference Search STATUS
-                byteString = encodeCommand(1,13,2,obj.ax,int32(0));
+                byteString = encodeCommand(1,13,2,obj.axis,int32(0));
                 byteString = obj.writeread(byteString);
                 [~,~,~,~,value] = decodeReply(byteString);
                 if value==0
@@ -44,7 +47,7 @@ classdef StepperMotor
                 java.lang.Thread.sleep(100)
             end
             % 3.7.13 Reference Search STOP
-            byteString = encodeCommand(1,13,1,obj.ax,int32(0));
+            byteString = encodeCommand(1,13,1,obj.axis,int32(0));
             obj.writeread(byteString);
             if nargout, success = false;
             else, warning("Time limit exceeded."), end
@@ -52,13 +55,13 @@ classdef StepperMotor
         function Set(obj,param,value)
             param = checkAxisParameter(param,value);
             % 3.7.5 Set Axis Parameter
-            byteString = encodeCommand(1,5,param,obj.ax,int32(value));
+            byteString = encodeCommand(1,5,param,obj.axis,int32(value));
             obj.writeread(byteString);
         end
         function success = SetPosition(obj,pos,timeout)
             if nargin<3, timeout = 10; end
             % 3.7.4 Move to Position
-            byteString = encodeCommand(1,4,0,obj.ax,int32(pos));
+            byteString = encodeCommand(1,4,0,obj.axis,int32(pos));
             obj.writeread(byteString);
             tic
             java.lang.Thread.sleep(50)
@@ -85,7 +88,7 @@ classdef StepperMotor
         function value = Get(obj,param)
             param = checkAxisParameter(param);
             % 3.7.6 Get Axis Parameter
-            byteString = encodeCommand(1,6,param,obj.ax,int32(0));
+            byteString = encodeCommand(1,6,param,obj.axis,int32(0));
             reply = obj.writeread(byteString);
             [~,~,~,~,value] = decodeReply(reply);
         end
@@ -95,15 +98,23 @@ classdef StepperMotor
     end
     methods (Access = private)
         function write(obj,byteString)
-            obj.sp.write(byteString,"uint8");
+            global StepperMotorPortMap %#ok<GVMIS> 
+            sp = StepperMotorPortMap(obj.port);
+            sp.write(byteString,"uint8");
         end
         function byteString = read(obj)
-            byteString = obj.sp.read(9,"uint8");
+            global StepperMotorPortMap %#ok<GVMIS> 
+            sp = StepperMotorPortMap(obj.port);
+            byteString = sp.read(9,"uint8");
         end
         function byteString = writeread(obj,byteString)
-            obj.sp.write(byteString,"uint8");
-            byteString = obj.sp.read(9,"uint8");
+            global StepperMotorPortMap %#ok<GVMIS> 
+            sp = StepperMotorPortMap(obj.port);
+            sp.write(byteString,"uint8");
+            byteString = sp.read(9,"uint8");
         end
+    end
+    methods (Static)
     end
 end
 % byteString = encodeCommand(module,command,type,motor,value)
